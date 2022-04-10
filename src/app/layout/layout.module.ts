d67acfa7fc28ba9d971e0915d8cf09f0c30e16e4
Adam Shields <adamenote@gmail.com>
2022-04-09 21:25:50 -0400

    added theme service and have configured

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatButtonModule,
+    FormsModule
+
   ],
   exports: [
     HeaderComponent,
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/theme.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isDarkMode: boolean;
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.initTheme();
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = this.themeService.isDarkMode();
+
+    this.isDarkMode
+      ? this.themeService.update('light-mode')
+      : this.themeService.update('dark-mode');
+  }
 
   ngOnInit(): void {
+    this.themeService.isDarkMode()
   }
 
 }
