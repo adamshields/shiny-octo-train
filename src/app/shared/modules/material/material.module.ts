41e20471742520e6550ea6cf4b64c0fee904a454
Adam Shields <adamenote@gmail.com>
2022-04-11 20:28:58 -0400

    Styling

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -15,9 +17,11 @@ import { FormsModule } from '@angular/forms';
   ],
   exports: [
     MatToolbarModule,
+    MatIconModule,
     MatSlideToggleModule,
     MatSidenavModule,
     MatDialogModule,
+    MatMenuModule,
     MatButtonModule,
     FormsModule
   ]
