a9aa7f4e37ac90417f508b1c479077f850ce9c52
Adam Shields <adamenote@gmail.com>
2022-04-16 16:01:59 -0400

    in memory db working for products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/modules/layout/page-not-found/page-not-found.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 const routes: Routes = [
      //Wild Card Route for 404 request
      { path: 'adam', pathMatch: 'full', component: PageNotFoundComponent },
+     { path: 'product', pathMatch: 'full', component: ProductListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './core/modules/layout/layout.module';
+import { ProductsModule } from './products/products.module';
 import { MaterialModule } from './shared/modules/material/material.module';
+import { DataService } from './shared/services/data.service';
 
 
 @NgModule({
@@ -19,6 +23,10 @@ import { MaterialModule } from './shared/modules/material/material.module';
     AppRoutingModule,
     MaterialModule,
     LayoutModule,
+    HttpClientModule,
+    ProductsModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
